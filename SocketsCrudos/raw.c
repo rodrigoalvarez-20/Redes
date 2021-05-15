a52a25c6f7774734826d47d7aaa27246930abf91
Rodrigo Alvarez <rodrigoalvarez449@gmail.com>
2021-05-15 16:44:03 -0500

    Cambios menores

--- SocketsCrudos/raw.c
+++ SocketsCrudos/raw.c
@@ -8,11 +8,6 @@
 #include <sys/ioctl.h>
 #include <net/if.h>
 #include <string.h>
-#include <sys/time.h>
-
-void displayFrame(unsigned char *, int);
-void displayMac(unsigned char[], int);
-void displayDir(unsigned char[], int);
 
 unsigned char 
     MAC_Origen[6], 
@@ -20,15 +15,14 @@ unsigned char
     IP_ADDR[4],
     frameToSend[1514],
     rFrame[1514],
-    ETH_TYPE[2] = {0x08, 0x06},
+    ETH_TYPE[2] = {0x0c, 0x0c},
     MAC_Broad[6] = {0xff, 0xff,0xff,0xff,0xff,0xff},
     FRAME_SEND[2] = {0x00, 0x01},
     FRAME_RECV[2] = {0x00, 0x02},
     P_TYPE[2] = {0x08, 0x00},
     HW_LEN[1] = {0x06},
     P_LEN[1] = {0x04},
-    HW_TYPE[2] = {0x00, 0x01},
-    IP_DST[4];
+    HW_TYPE[2] = {0x00, 0x01};
 int interface_index;
 
 
@@ -54,8 +48,8 @@ int main(){
     getData(packet_socket);
     setFrame(frameToSend);
     sendFrame(packet_socket, frameToSend);
-    receiveFrame(packet_socket, rFrame);
-
+    receiveFrame(packet_socket, rFrame);    
+    
     close(packet_socket);
 
     return 0;
@@ -79,8 +73,9 @@ void getData(int socket){
         exit(-1);
     }
     memcpy(MAC_Origen, nic.ifr_hwaddr.sa_data, 6);
-    //printf("Direccion MAC de origen: ");
-    //displayMac(MAC_Origen, 6);
+    printf("Direccion MAC de origen: ");
+    displayMac(MAC_Origen, 6);
+    printf("\n");
 
     if(ioctl(socket, SIOCGIFADDR, &nic) == -1){
         perror("Ha ocurrido un error al obtener la direcion IP de origen\n");
@@ -88,8 +83,9 @@ void getData(int socket){
     }
 
     memcpy(IP_ADDR, nic.ifr_addr.sa_data+2, 4);
-    //printf("Direccion IP de origen: ");
-    //displayDir(IP_ADDR, 4);
+    printf("Direccion IP de origen: ");
+    displayDir(IP_ADDR, 4);
+    printf("\n");
 
     if(ioctl(socket, SIOCGIFNETMASK, &nic) == -1){
         perror("Ha ocurrido un error al obtener la mascara de red\n");
@@ -97,8 +93,9 @@ void getData(int socket){
     }
 
     memcpy(NET_MASK, nic.ifr_netmask.sa_data+2, 4);
-    //printf("Mascara de red: ");
-    //displayDir(NET_MASK, 4);
+    printf("Mascara de red: ");
+    displayDir(NET_MASK, 4);
+    printf("\n");
 
 }
 
@@ -106,30 +103,7 @@ void setFrame(unsigned char *frame){
     memcpy(frame+0, MAC_Broad, 6); //MAC destino - Broadcast
     memcpy(frame+6, IP_ADDR, 6); //MAC origen
     memcpy(frame+12, ETH_TYPE, 2); //Tipo de trama
-    memcpy(frame+14, HW_TYPE, 2); //Tipo de HW
-    memcpy(frame+16, P_TYPE, 2); //Tipo de Protocolo
-    memcpy(frame+18, HW_LEN, 1); //Tamanio de Hw
-    memcpy(frame+19, P_LEN, 1); //Tamanio de Protocolo
-    memcpy(frame+20, FRAME_SEND, 2); //Tipo de Operacion
-    memcpy(frame+22, MAC_Origen, 6); //MAC origen
-    memcpy(frame+28, IP_ADDR, 4); //IP Origen
-    memcpy(frame+32, MAC_Broad, 6); //MAC Destino - Broadcast
-    unsigned char ip[20];
-    int pIp[4];
-    printf("Introduce la direccion IP de destino: ");
-    scanf("%s", ip);
-
-    char *token = strtok(ip, ".");
-    int cont = 0;
-    while(token != NULL){
-        IP_DST[cont] = atoi(token);
-        token = strtok(NULL, ".");
-        cont++;
-    }
-    memcpy(frame+38, IP_DST, 4); //IP a descubrir
-    printf("Trama a enviar: \n");
-    displayFrame(frame, 42);
-    printf("\n");
+    memcpy(frame+14, "Rodrigo Alvarez Perez", 30);    
 }
 
 void sendFrame(int socket, unsigned char *frame){
@@ -149,32 +123,6 @@ void sendFrame(int socket, unsigned char *frame){
 
 }
 
-void receiveFrame(int socket, unsigned char *frame){
-    while(1){
-        int tam = recvfrom(socket, frame, 1514, 0, NULL, 0);
-        if(tam != -1){
-            if( !memcmp(frame, MAC_Origen, 6) && !memcmp(frame+20, FRAME_RECV, 2) && !memcmp(frame+28, IP_DST, 4)){
-                unsigned char ip[4], mac[6];
-                memcpy(mac, frame+22, 6);
-                memcpy(ip, frame+28, 4);
-                printf("Trama obtenida: \n");
-                displayFrame(frame, tam);
-                printf("Direccion IP: ");
-                displayDir(ip, 4);
-                printf("\n");
-                printf("Direccion MAC: ");
-                displayMac(mac, 6);
-                printf("\n");
-                break;
-            }
-        }else {
-            perror("Ha ocurrido un error al obtener los datos\n");
-            exit(-1);
-        }
-    }
-    
-}
-
 void displayFrame(unsigned char *frame, int tam){
     for(int i = 0; i < tam; i++){
         if(i % 16 == 0){
@@ -183,6 +131,16 @@ void displayFrame(unsigned char *frame, int tam){
         printf("%.2x ", frame[i]);
     }
     printf("\n");
+}
+
+void receiveFrame(int socket, unsigned char *frame){
+    int tam = recvfrom(socket, frame, 1514, 0, NULL, 0);
+    if(tam != -1){
+        displayFrame(frame, tam);
+    }else {
+        perror("Ha ocurrido un error al obtener los datos\n");
+        exit(-1);
+    }
 }
 
 void displayMac(unsigned char MAC[6], int size){
